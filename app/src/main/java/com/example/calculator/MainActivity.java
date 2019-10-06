719dfd430b9c11a74d599cf4a79f1f6bd211ed68
Yash Parekh <parekhyashj@gmail.com>
2019-10-06 14:16:18 -0400

    Proper layout, colors and hidden actionbar

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        git demo comment
-
+//        TO HIDE THE ACTION BAR ON TOP
+        getSupportActionBar().hide();
     }
 }
